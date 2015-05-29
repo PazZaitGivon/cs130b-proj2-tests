448b80b87d0978995ff5e3ba9d0efb208c091604
Mark Nguyen <mpnguyen@umail.ucsb.edu>
2015-05-28 20:39:36 -0700

    Colorized error codees

--- test.sh
+++ test.sh
@@ -1,9 +1,16 @@
 #! /bin/bash
+
+
+errorColor="\033[0;31m"
+noColor="\033[0m"
+
 for i in {1..13}; do
     echo "Testing input.$i"
     rm tmp.txt -f
     ../project < "input.$i" &> tmp.txt
+    printf "$errorColor"
     diff tmp.txt "output.$i"
+    printf "$noColor"
 done
 
 rm tmp.txt -f
